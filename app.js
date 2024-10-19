4978e01a55484a68dd2e5e108cb7b88e779f7091
freddy990117 <freddy990117@gmail.com>
2024-10-19 12:21:26 +0800

    Update Application

--- app.js
+++ app.js
@@ -8,20 +8,20 @@ const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 
-// initial showMovie
+// initinal URL
 getMovie(API_URL);
 
 async function getMovie(url) {
-  const res = await fetch(url);
-  const data = await res.json();
-  showMovie(data.results);
+  const response = await fetch(url);
+  const movieData = await response.json();
+  showMovie(movieData.results);
 }
 
-// ShowMovies
+function showMovie(movieData) {
+  main.innerHTML = ""; // 清空舊的內容
 
-function showMovie(movies) {
-  movies.forEach((movie) => {
-    const { poster_path, title, vote_average, overview } = movie;
+  movieData.forEach((movie) => {
+    const { title, poster_path, overview, vote_average } = movie;
 
     const newDiv = document.createElement("div");
     newDiv.classList.add("movie");
@@ -29,31 +29,28 @@ function showMovie(movies) {
     newDiv.innerHTML = `
         <img src="${IMG_PATH + poster_path}" alt="${title}" />
         <div class="movie-info">
-          <h3>${title}</h3>
-          <span class="${getAverageColor(vote_average)}">${vote_average}</span>
+            <h3>${title}</h3>
+        <span class="${voteAverage(vote_average)}">${vote_average}</span>
         </div>
-        <div class="overview">
-          <h3>Overview</h3>
-          ${overview}
+             <div class="overview">
+              <h3>${overview}</h3>
         </div>
-    `;
+          `;
 
     main.appendChild(newDiv);
   });
 }
 
-// voteAverage's color
-function getAverageColor(vote) {
-  if (vote > 7) {
+function voteAverage(vote_average) {
+  if (vote_average > 7) {
     return "green";
-  } else if (vote > 4) {
+  } else if (vote_average > 5) {
     return "orange";
   } else {
     return "red";
   }
 }
 
-// Create addEventListener for Search
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
